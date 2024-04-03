e01780cc7d7043799a7ab936fbb1e1f6558d8df9
ShoxruxAsadov <shoxruxasadov05@mail.ru>
2024-04-04 03:51:49 +0500

    test auth google

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -19,5 +19,11 @@ export default NextAuth({
             console.log("end", session, token, user);
             return session;
         },
+        async signIn({ account, profile }) {
+            if (account.provider === "google") {
+                return profile.email_verified && profile.email.endsWith("@example.com")
+            }
+            return true // Do different verification for other providers that don't have `email_verified`
+        },
     },
 })

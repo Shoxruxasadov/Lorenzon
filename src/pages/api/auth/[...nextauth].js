33cc1fec96f6bbefb860f8ababb41f50b12a694b
ShoxruxAsadov <shoxruxasadov05@mail.ru>
2024-04-04 02:51:44 +0500

    add callbacks to next auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -10,5 +10,14 @@ export default NextAuth({
     ],
     pages: {
         signIn: '/login'
-    }
+    },
+    callbacks: {
+        async session({ session, token, user }) {
+            console.log("callback", session, token, user);
+            session.user.username = session.user.name
+            session.user.uid = token.sub;
+            console.log("end", session, token, user);
+            return session;
+        },
+    },
 })

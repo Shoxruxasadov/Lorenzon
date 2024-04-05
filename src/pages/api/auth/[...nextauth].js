fd2978f0d6e3a26233344b5c518febfb5e0b403d
ShoxruxAsadov <shoxruxasadov05@mail.ru>
2024-04-05 15:27:44 +0500

    remove callbacks in nextauth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -11,14 +11,5 @@ export default NextAuth({
     pages: {
         signIn: '/login'
     },
-    callbacks: {
-        async session({ session, token, user }) {
-            console.log("callback", session, token, user);
-            session.user.username = session.user.name
-            session.user.uid = token.sub;
-            console.log("end", session, token, user);
-            return session;
-        },
-    },
-    secret: process.env.NEXTAUTH_SECRET
+    secret: process.env.NEXTAUTH_SECRET,
 })

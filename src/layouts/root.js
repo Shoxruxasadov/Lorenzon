c4763f4b37f45bef50e05a1ba27efcee4a55036f
ShoxruxAsadov <shoxruxasadov05@mail.ru>
2024-04-05 14:56:30 +0500

    robots txt

--- src/layouts/root.js
+++ src/layouts/root.js
@@ -10,8 +10,10 @@ export default function Root({ children, page, title }) {
                 <link href="/manifest.json" rel="manifest" />
                 <meta name="theme-color" content="#0A0D13" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="google-site-verification" content="E4VKDILHxyNIMCC_ESpZ2l7q_h_Dg-S90tpF_W8vjVg" />
                 <meta name="description" content="Lorenzon • Enjoy the music and connect with people • Free Music Player" />
+                <meta name="keywords" content="Lorenzon, Lorenzonuz, Lorenzon uz, Lorenzon bot, Lorenzo, lorenzon, Music, Player, Free, Spotify, Spotify clone, Streaming, Stream, Streaming Music Player, Music Player, Song, Songs, Song player, Sound, Sound player, SoundCloud, SoundCloud clone" />
             </Head>
             <div id={page}>
                 {children}

6a1a47ee14816c91bb90cfeacdba11796f8ad760
Shoxrux Asadov <129892167+Shoxruxasadov@users.noreply.github.com>
2024-05-10 03:45:03 +0500

    FINISH

--- src/components/loading/wait.js
+++ src/components/loading/wait.js
@@ -2,7 +2,7 @@ import Root from "../../layouts/root";
 
 export default function Wait() {
     return (
-        <Root page="wait" title={window.location.pathname == '/' ? 'Enjoy the music • Music streaming service' :'Loading...'}>
+        <Root page="wait" title={(window && window.location.pathname == '/') ? 'Enjoy the music • Music streaming service' :'Loading...'}>
             <span className="loader"></span>
         </Root>
     )
